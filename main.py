5336da51d77909a2d78a68b04460c8ec7c190ce8
lvsanrdstrm <lvsanrdstrm@gmail.com>
2024-02-05 14:14:26 +0100

    lagt till common characters och common descriptors-funktioner. search any books har börjat lagga med metamorphosis dock.

--- main.py
+++ main.py
@@ -27,26 +27,9 @@ def add_user_input():
     # Add pattern to the matcher
     matcher.add("user_pattern", [pattern])
 
-    matches = matcher(metamDoc)
-    
-    matched_phrase_sentence = []
-
-    for match_id, start, end in matches:
-        matched_phrase = metamDoc[start:end].text
-        for sent in metamDoc.sents:
-            if start >= sent.start and end <= sent.end:
-                matched_sentence = sent.text
-                matched_phrase_sentence.append({"phrase": matched_phrase, "sentence": matched_sentence})
-
-    # Print the result
-    for match in matched_phrase_sentence:
-        print("Matched phrase:", match["phrase"])
-        print("Sentence:", match["sentence"])
-        print()
-
-    
 
 def search_metam(): 
+    add_user_input()
     # Perform matching
     matches = matcher(metamDoc)
 
@@ -66,7 +49,12 @@ def search_metam():
         print("Sentence:", match["sentence"])
         print()
 
+
+    matcher.remove("user_pattern")       
+
 def search_yellowp():
+    add_user_input()
+
     matches = matcher(yellowpDoc)
 
 
@@ -86,28 +74,92 @@ def search_yellowp():
         print("Sentence:", match["sentence"])
         print()
 
-def get_wallpaper_sentences():
-    pattern = [{"TEXT": "wallpaper"}]
-    matcher.add("sun_pattern", [pattern])
+    matcher.remove("user_pattern")   
 
-    matches = matcher(yellowpDoc)
+def search_any_book():
+    add_user_input()
 
+    to_search = input("Do you want to search through Metamorphosis by Franz Kafka or The Yellow Wallpaper by Charlotte Perkins? M/Y:  ").lower()
+    while True:
+        if to_search == "m":
+            print("You chose Metamorphosis.")
+            doc_to_search = metamDoc
+            break
+        elif to_search == "y":
+            print("You chose The Yellow Wallpaper.")
+            doc_to_search = yellowpDoc    
+            break
+        else:
+            print("Invalid entry. M for Metamorphosis and Y for The Yellow Wallpaper.")
+
+    matches = matcher(doc_to_search)
+    # iterate through the sentences and check if start and end of phrase in in sentence
+    matched_phrase_sentence = []
+
+    for match_id, start, end in matches:
+        matched_phrase = doc_to_search[start:end].text
+        for sent in doc_to_search.sents:
+            if start >= sent.start and end <= sent.end:
+                matched_sentence = sent.text
+                matched_phrase_sentence.append({"phrase": matched_phrase, "sentence": matched_sentence})
+
+    # Print the result
+    for match in matched_phrase_sentence:
+        print("Matched phrase:", match["phrase"])
+        print("Sentence:", match["sentence"])
+        print()
+   
+
+    matcher.remove("user_pattern")
+
+def common_characters():
+    to_search = input("Do you want to search through Metamorphosis by Franz Kafka or The Yellow Wallpaper by Charlotte Perkins? M/Y:  ").lower()
+    while True:
+            if to_search == "m":
+                doc_to_search = metamDoc
+                break
+            elif to_search == "y":
+                doc_to_search = yellowpDoc    
+                break
+            else:
+                print("Invalid entry. M for Metamorphosis and Y for The Yellow Wallpaper.")
+
+    for ent in doc_to_search.ents:
+        if str(ent.label_) == "PERSON":
+            print(ent.text, '\t',ent.label_)
+
+def common_descriptions():
+    to_search = input("Do you want to search through Metamorphosis by Franz Kafka or The Yellow Wallpaper by Charlotte Perkins? M/Y:  ").lower()
+    while True:
+            if to_search == "m":
+                doc_to_search = metamDoc
+                break
+            elif to_search == "y":
+                doc_to_search = yellowpDoc    
+                break
+            else:
+                print("Invalid entry. M for Metamorphosis and Y for The Yellow Wallpaper.")
+
+    for token in doc_to_search:
+        if token.pos_ == "ADJ":
+            print(token.text, '\t', token.pos_)      
+
+'''
+def asking_mode():
+    print("You will be able to ask about the main characters, the most common locations and the most common descriptive word of the books.")
+    question1 = input("Do you want to get information about the most mentioned characters? Y/N: ").lower()
+    if question1 == "y":
+        common_characters()
+        question2 = input("Do you want to get information about the most mentioned locations? Y/N: ").lower()
+        if question2 == "y":
+            common_locations()
+        else:
+            print("Skipping most common locations-information.")
+    else:
+        print("Skipping most common characters-information.")'''
     
-    # iterate through the sentences and check if start and end of phrase in in sentence
-    matched_phrase_sentence = []
 
-    for match_id, start, end in matches:
-        matched_phrase = yellowpDoc[start:end].text
-        for sent in yellowpDoc.sents:
-            if start >= sent.start and end <= sent.end:
-                matched_sentence = sent.text
-                matched_phrase_sentence.append({"phrase": matched_phrase, "sentence": matched_sentence})
 
-    # Print the result
-    for match in matched_phrase_sentence:
-        print("Matched phrase:", match["phrase"])
-        print("Sentence:", match["sentence"])
-        print()
 
 
 
--- menu.py
+++ menu.py
@@ -5,22 +5,32 @@ while run:
   answer = input("\nMenu\n"
                  "\n Welcome to LitSearch2000."
                  "\n This program allows you to search two sources for matches on your chosen search phrase."
-                 "\n The result will be ordered according to accuracy."
-                  "\n1. Enter search word and search through Metamorphosis for matches(temporarily)."
-                  "\n2. Print sentences with the word wallpaper."
+                # "\n The result will be ordered according to accuracy."
+                  "\n1. Search Metamorphosis with your chosen search phrase."
+                  "\n2. Search The Yellow Wallpaper with your chosen search phrase."
+                  "\n3. Search Metamorphosis or The Yellow Wallpaper with your chosen search phrase."
+                  "\n4. Ask LitSearch2000 about the most common characters in the books."
+                  "\n5. Ask LitSearch2000 about the most common descriptive words in the books."
                   "\nQ. Shut down LitSearch2000"
                    "\n-> ").strip()
   match answer.lower():
         case "1":
-            print("Enter search word.")
-            # länka till funktion i main där input sker
-            main.add_user_input()
-        case "2":    
-            print("Printing sentences with the word wallpaper")
-            main.get_wallpaper_sentences()
+          print("Welcome to Metamorphosis-search")
+          main.search_metam()  
+        case "2":
+          print("Welcome to The Yellow Wallpaper-search") 
+          main.search_yellowp()
+        case "3":
+          print("Here you can search any of the books.") 
+          main.search_any_book()
+        case "4":
+          print("Entering asking-mode")
+          main.common_characters()
+        case "5":
+          main.common_descriptions()
         case "q":
-            print("Shutting down LitSearch2000")
-            run = False
+          print("Shutting down LitSearch2000")
+          run = False
         case _:
-            print(f"'{answer}' is not an option. Choose between 1-2 or Q!")
+          print(f"'{answer}' is not an option. Choose between 1-4 or Q!")
 
